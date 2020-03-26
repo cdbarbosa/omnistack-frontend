6fc44f3286fba73422718f6b968289c589896be0
cdbarbosa <cdbarbosa@atlassian.com>
2020-03-26 08:45:36 -0300

    finalizaçao do frontend

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,12 +1,50 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
 
 import "./style.css";
 import logoImg from "../../assets/logo.svg";
 import { FiPower, FiTrash2 } from "react-icons/fi";
+import api from "../../services/api";
 
 export default function Profile() {
   const ongName = localStorage.getItem("ongName");
+  const ongId = localStorage.getItem("ongId");
+  const [incidents, setIncidents] = useState([]);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    api
+      .get("profile", {
+        headers: {
+          Authorization: ongId
+        }
+      })
+      .then(response => {
+        setIncidents(response.data);
+      });
+  }, [ongId, ongName]);
+
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId
+        }
+      });
+
+      setIncidents(incidents.filter(incident => incident.id !== id));
+    } catch (error) {
+      alert("Erro ao deletar caso, tente novamente");
+    }
+  }
+
+  function handleLogout() {
+    localStorage.clear();
+
+    history.pushState("/");
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -16,7 +54,7 @@ export default function Profile() {
         <Link className="button" to="/new">
           Cadastrar novo caso
         </Link>
-        <button type="button">
+        <button type="button" onClick={handleLogout}>
           <FiPower size={18} color="#e02041"></FiPower>
         </button>
       </header>
@@ -24,62 +62,30 @@ export default function Profile() {
       <h1>Casos Cadastrados</h1>
 
       <ul>
-        <li>
-          <strong>Caso: </strong>
-          <p>Caso teste</p>
+        {incidents.map(incident => (
+          <li key={incident.id}>
+            <strong>Caso: </strong>
+            <p>{incident.title}</p>
 
-          <strong>Descrição:</strong>
-          <p>Descrição teste</p>
+            <strong>Descrição:</strong>
+            <p>{incident.description}</p>
 
-          <strong>Valor:</strong>
-          <p>R$ 120,00</p>
+            <strong>Valor:</strong>
+            <p>
+              {Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL"
+              }).format(incident.value)}
+            </p>
 
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
-          </button>
-        </li>
-        <li>
-          <strong>Caso: </strong>
-          <p>Caso teste</p>
-
-          <strong>Descrição:</strong>
-          <p>Descrição teste</p>
-
-          <strong>Valor:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
-          </button>
-        </li>
-        <li>
-          <strong>Caso: </strong>
-          <p>Caso teste</p>
-
-          <strong>Descrição:</strong>
-          <p>Descrição teste</p>
-
-          <strong>Valor:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
-          </button>
-        </li>
-        <li>
-          <strong>Caso: </strong>
-          <p>Caso teste</p>
-
-          <strong>Descrição:</strong>
-          <p>Descrição teste</p>
-
-          <strong>Valor:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
-          </button>
-        </li>
+            <button
+              type="button"
+              onClick={() => handleDeleteIncident(incident.id)}
+            >
+              <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
